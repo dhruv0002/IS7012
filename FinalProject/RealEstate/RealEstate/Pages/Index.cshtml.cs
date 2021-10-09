52fbc8129b65325f564c73c33a0db0894690ad29
dhruv0002 <dhruv0002@gmail.com>
2021-10-09 18:05:45 -0400

    Added Statistics on the main index page(homepage))

--- FinalProject/RealEstate/RealEstate/Pages/Index.cshtml.cs
+++ FinalProject/RealEstate/RealEstate/Pages/Index.cshtml.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
@@ -12,14 +13,25 @@ namespace RealEstate.Pages
     {
         private readonly ILogger<IndexModel> _logger;
 
-        public IndexModel(ILogger<IndexModel> logger)
+        private readonly RealEstate.Data.RealEstateContext _context;
+
+        public IndexModel(ILogger<IndexModel> logger, RealEstate.Data.RealEstateContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
-        public void OnGet()
-        {
+        public int TotalAgents { get; set; }
+        public int TotalDwellings { get; set; }
+        public int TotalCities { get; set; }
+        public int TotalSalesOffices { get; set; }
 
+        public async Task OnGetAsync()
+        {
+            TotalAgents = await _context.Agent.CountAsync();
+            TotalDwellings = await _context.Dwelling.CountAsync();
+            TotalCities = await _context.City.CountAsync();
+            TotalSalesOffices = await _context.SalesOffice.CountAsync();
         }
     }
 }
